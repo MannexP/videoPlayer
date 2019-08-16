8866ba2a1fa060772d6f397e24b9be0d1e68f457
Mannex Pimentel <mannex247@gmail.com>
2019-08-15 23:25:52 -0400

    save"

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-
 import youtube from '../apis/youtube';
 
 
 class App extends React.Component {
+    state = { videos: [] };
 
-    onTermSubmit = (term) =>{
-        console.log(term);
+    onTermSubmit = async term =>{
+       const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+
+      this.setState({videos: response.data.items})
     };
 
 
@@ -16,6 +22,8 @@ class App extends React.Component {
             <div className="ui container">
 
             <SearchBar onFormSubmit={this.onTermSubmit}/>
+
+            I have {this.state.videos.length} videos.
             
             </div>
 
